a65b55321da9c8763d40a13102a0b569dca60356
Clittrell13 <159974860+Clittrell13@users.noreply.github.com>
2024-07-03 16:32:53 -0700

    Create change.py

---
+++ change.py
@@ -0,0 +1,16 @@
+cents = int(input("Please enter an amount in cents less than a dollar: "))
+quarters = cents // 25
+cents %= 25
+
+dimes = cents // 10
+cents %= 10
+
+nickels = cents // 5
+cents %= 5
+
+pennies = cents
+
+print(f"Q: {quarters},\n"
+      f"D: {dimes},\n"
+      f"N: {nickels},\n"
+      f"P: {pennies}")
